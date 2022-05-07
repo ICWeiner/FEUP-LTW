e5e4f86021c279254d4c0bb12be6e6227157e74a
Diogo Nunes <diogoffn@gmail.com>
2022-05-07 07:01:37 -0700

    reworked login/register layout

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Brand Spanking NEW Unter Eats</title>
 		<link href="forms.css" rel="stylesheet">
 	</head>
@@ -9,21 +10,23 @@
 		<header>
 			<h1>NEW Unter Eats</h1>
             <!-- <a href="about.php">About</a> -->
-			<div id="signup">
+			<div id="signup"><!-- TO-DO: mudar este div para nav? -->
 				<a href="login.php">Login</a>
 				<a href="register.php">Register</a>
 			</div>
 		</header>
-		<img src="TestImages/mainImage.jpeg" alt="Homepage image of new unter eats" id="mainImage">
-		<h2>Discover amazing restaurants</h2>
 		<main>
-			<h2>Login</h2>
-			<form action="action_login.php" method="post" class="login">
-				<input type="text" name="username" placeholder="Username">
-				<input type="password" name="password" placeholder="Password">
-				<button type="submit">Login</button>
-				<a href="register.php">Dont have an account yet? Register here</a>
-			</form>
+			<img src="TestImages/mainImage.jpeg" alt="Homepage image of new unter eats" id="mainImage">
+			<div id="">
+				<h3>Discover amazing restaurants</h3>
+				<h2>Login</h2>
+				<form action="action_login.php" method="post" class="login">
+					<input type="text" name="username" placeholder="Username">
+					<input type="password" name="password" placeholder="Password">
+					<button type="submit">Login</button>
+					<a href="register.php">Dont have an account yet? Register here</a>
+				</form>
+			</div>
 		</main>
 	</body>
 </html>
