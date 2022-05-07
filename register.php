a309ec887b37c4a4a5dc857e2cbef455b5129a76
Diogo Nunes <diogoffn@gmail.com>
2022-05-07 07:22:21 -0700

    created template files for login/register

--- register.php
+++ register.php
@@ -1,35 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Brand Spanking NEW Unter Eats</title>
-		<link href="forms.css" rel="stylesheet">
-	</head>
-	<body>
-		<header>
-			<h1>NEW Unter Eats</h1>
-            <!-- <a href="about.php">About</a> -->
-			<div id="signup"><!-- TO-DO: mudar este div para nav? -->
-				<a href="login.php">Login</a>
-				<a href="register.php">Register</a>
-			</div>
-		</header>
-		<main>
-			<img src="TestImages/mainImage.jpeg" alt="Homepage image of new unter eats" id="mainImage">
-			<div id="">
-				<h3>Discover amazing restaurants</h3>
-				<h2>Register</h2>
-				<form action="action_register.php" method="post" class="register">
-					<input type="text" name="username" placeholder="Username">
-					<input type="password" name="password" placeholder="Password">
-					<input type="password" name="confirm_password" placeholder="Confirm password">
-					<input type="text" name="address" placeholder="Address">
-					<input type="tel" name="phone" placeholder="Phone number">
-					<button type="submit">Register</button>
-					<a href="login.php">Already have an account? Login here</a>
-				</form>
-			</div>
-		</main>
-	</body>
-</html>
+<?php 
+	declare(strict_types = 1); 
+
+	require_once('templates/authCommon.tpl.php');
+
+	drawHeader();
+	drawRegister();
+
+?>
---
+++ templates/authCommon.tpl.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1); ?>
+
+<?php function drawHeader() { ?>
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Brand Spanking NEW Unter Eats</title>
+			<link href="forms.css" rel="stylesheet">
+		</head>
+		<body>
+			<header>
+				<h1>NEW Unter Eats</h1>
+	            <!-- <a href="about.php">About</a> -->
+				<div id="signup"><!-- TO-DO: mudar este div para nav? -->
+					<a href="login.php">Login</a>
+					<a href="register.php">Register</a>
+				</div>
+			</header>
+
+<?php } ?>
+
+<?php function drawLogin() { ?>
+			<main>
+				<img src="TestImages/mainImage.jpeg" alt="Homepage image of new unter eats" id="mainImage">
+				<div id="">
+					<h3>Discover amazing restaurants</h3>
+					<h2>Login</h2>
+					<form action="action_login.php" method="post" class="login">
+						<input type="text" name="username" placeholder="Username">
+						<input type="password" name="password" placeholder="Password">
+						<button type="submit">Login</button>
+						<a href="register.php">Dont have an account yet? Register here</a>
+					</form>
+				</div>
+			</main>
+		</body>
+	</html>
+<?php } ?>
+
+
+<?php function drawRegister() { ?>
+			<main>
+				<img src="TestImages/mainImage.jpeg" alt="Homepage image of new unter eats" id="mainImage">
+				<div id="">
+					<h3>Discover amazing restaurants</h3>
+					<h2>Register</h2>
+					<form action="action_register.php" method="post" class="register">
+						<input type="text" name="username" placeholder="Username">
+						<input type="password" name="password" placeholder="Password">
+						<input type="password" name="confirm_password" placeholder="Confirm password">
+						<input type="text" name="address" placeholder="Address">
+						<input type="tel" name="phone" placeholder="Phone number">
+						<button type="submit">Register</button>
+						<a href="login.php">Already have an account? Login here</a>
+					</form>
+				</div>
+			</main>
+		</body>
+	</html>
+<?php } ?>
