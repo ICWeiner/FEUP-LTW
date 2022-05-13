814ee1e8f8de202cb8a7d75bc77df1432ba79092
Diogo Nunes <38764693+ICWeiner@users.noreply.github.com>
2022-05-13 08:19:25 -0700

    removed unused html code in cart.php

--- cart.php
+++ cart.php
@@ -13,28 +13,3 @@
     drawCart($orders);
     drawFooter();
 ?>
-
-<!--<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/style.css">
-        <link href="css/layout.css" rel="stylesheet">
-        <title>Brand Spanking NEW Unter Eats</title>
-    </head>
-<body>
-
-    <header>
-            <h1>NEW Unter Eats</h1>
-            <a href="cart.php">Cart
-            <img src="./TestImages/cartIcon.png" alt="cart icon"></a>
-            <p>Nome de utilizador</p>
-    </header>
-    
-    <footer>
-        <p>Copyright &copy; LTW Project, 2022</p>
-    </footer>
-    
-</body>
-</html>
--->
