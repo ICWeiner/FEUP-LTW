1ff8b4602a0d05e13d9102408698007929106197
Diogo Nunes <diogoffn@gmail.com>
2022-05-13 09:34:43 -0700

    fixed footer spacing

--- profile.php
+++ profile.php
@@ -4,10 +4,10 @@
 	require_once('templates/common.tpl.php');
 
 	//TODO: take this array out and read this data from DB instead
-	$users = array('name'=> 'Diogo','phone'=> '12321424','email'=> 'diogo@gmail.com','address'=> 'Wouldnt you like to know, weather boy', 'postcode'=> '1234-567', 'country'=> 'Portugal, most of the time' );
+	$user = array('name'=> 'Diogo','phone'=> '12321424','email'=> 'diogo@gmail.com','address'=> 'Wouldnt you like to know, weather boy', 'postcode'=> '1234-567', 'country'=> 'Portugal, most of the time' );
 
 	drawHeader();
-	drawProfile($users);
+	drawProfile($user);
 	drawFooter();
 ?>
 
