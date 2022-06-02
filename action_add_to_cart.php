9e64f8bf11a3313fc131589c722f447da2fed50e
Anete <anetempereira@gmail.com>
2022-06-02 16:05:05 +0100

    started action page to add to cart

---
+++ action_add_to_cart.php
@@ -0,0 +1,20 @@
+<?php
+  declare(strict_types = 1);
+
+  session_start();
+
+  if (!isset($_SESSION['id'])) die(header('Location: /'));
+
+  require_once('database/connection.db.php');
+  require_once('database/cart.class.php');
+
+  $db = getDatabaseConnection();
+
+  $id = intval($_GET['id']);
+
+  if (!isset($_SESSION['cart'][$id])) 
+        $_SESSION['cart'][$id] = 0;
+  $_SESSION['cart'][$id]++;   //aumentar a quantidade de coisas no carrinho para aquele id 
+
+  header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
