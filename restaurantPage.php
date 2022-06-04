a2fde6717a76bed8c30f90db5994d242729e4d3e
Diogo Nunes <diogoffn@gmail.com>
2022-06-04 01:43:55 -0700

    added a TODO

--- restaurantPage.php
+++ restaurantPage.php
@@ -1,36 +1,36 @@
 <?php 
-    declare(strict_types = 1);
+	declare(strict_types = 1);
 
-    session_start();
+	session_start();
 
-    if(!isset($_SESSION['name']) ){
-        header('Location: login.php');
-    }
+	if(!isset($_SESSION['name']) ){
+		header('Location: login.php');
+	}
 
-    require_once('database/connection.db.php');
-    require_once('database/dish.class.php');
-    require_once('templates/common.tpl.php');
-    require_once('templates/restaurant.tpl.php');
+	require_once('database/connection.db.php');
+	require_once('templates/common.tpl.php');
+	require_once('database/dish.class.php');
+	require_once('templates/restaurant.tpl.php');
 
 
-    $db = getDatabaseConnection();
-    $id = intval($_GET['id']);
+	$db = getDatabaseConnection();
+	$id = intval($_GET['id']);
 
-    $categories = Dish::getDishesCategories($db, $id);
+	$categories = Dish::getDishesCategories($db, $id);
 
-    drawHeader($_SESSION['name']);
+	drawHeader($_SESSION['name']);
 
-    //arranjar a query, esta so a retornar 1 restaurante :<
-    $stmt = $db->prepare('SELECT DISTINCT RestaurantName FROM Restaurant WHERE RestaurantId = ?');
-    $stmt->execute(array($id));
-    $row = $stmt->fetch();
+	//arranjar a query, esta so a retornar 1 restaurante :< TODO why the FUCK is this here????????????????????
+	$stmt = $db->prepare('SELECT DISTINCT RestaurantName FROM Restaurant WHERE RestaurantId = ?');
+	$stmt->execute(array($id));
+	$row = $stmt->fetch();
 
-    drawDishesRestaurantName($row['RestaurantName']);
-    
-    foreach($categories as $category){
-        $dishes = Dish::getRestaurantDishesByCategory($db, $category['name'],$id);
-        drawDishesByCategory($dishes,$category['name']);
-    }
+	drawDishesRestaurantName($row['RestaurantName']);
+	
+	foreach($categories as $category){
+		$dishes = Dish::getRestaurantDishesByCategory($db, $category['name'],$id);
+		drawDishesByCategory($dishes,$category['name']);
+	}
 
-    drawFooter();
+	drawFooter();
 ?>
