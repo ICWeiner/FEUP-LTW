238b62c303df5d5e0dab9d69269ce56906a6bab9
Anete <anetempereira@gmail.com>
2022-06-06 19:03:07 +0100

    storing of orders in database finished

--- testingAjax.php
+++ testingAjax.php
@@ -1,7 +1,38 @@
 <?php
 
-$requestReceiver = file_get_contents("php://input");
-$object = json_decode($requestReceiver, true);
-var_dump($object);
+    require_once('database/connection.db.php');
+
+
+    $db = getDatabaseConnection();
+
+
+    $requestReceiver = file_get_contents("php://input");
+    $cart = json_decode($requestReceiver, true);
+
+    $total = $cart["total"];
+    $date = $cart["date"];
+    $state = $cart["status"];
+    $user = $cart["userId"];
+
+    $stmt = $db->prepare('INSERT INTO FoodOrder (OrderDate, OrderState, User) VALUES ( ?, ?, ?);
+    ');
+
+    $stmt->execute(array($date, $state, $user));
+    $orderId = $db->lastInsertId();
+
+    foreach ($cart["items"] as $dish){
+        $dishId = $dish["dishId"];
+        $quantity = $dish["quantity"];
+        $stmt = $db->prepare('INSERT INTO DishOrder (OrderId, DishId, quantity) VALUES ( ?,?, ?);
+        ');
+
+        $stmt->execute(array($orderId, $dishId, $quantity));
+    }
+
+    
+    
+
+
+    var_dump($cart);
 
 ?>
