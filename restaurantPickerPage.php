816a3160be662800873b8c414e548e7955cf066b
Diogo Nunes <diogoffn@gmail.com>
2022-06-13 10:52:03 -0700

    made users unable to access pages they shouldn´t

--- restaurantPickerPage.php
+++ restaurantPickerPage.php
@@ -7,6 +7,10 @@
         header('Location: login.php');
     }
 
+    if($_SESSION['type'] != "restaurantOwner"){
+        header('Location: home.php');
+    }
+
     require_once(__DIR__.'/database/connection.db.php');
 	require_once(__DIR__.'/templates/common.tpl.php');
     require_once(__DIR__.'/templates/addRestaurant.tpl.php');
