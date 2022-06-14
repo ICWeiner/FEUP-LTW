8f76cc7fcf68f2ca722f9cebf8f8b8edff12357e
Anete <anetempereira@gmail.com>
2022-06-14 10:53:33 +0100

    favorites are added and deleted from database

--- script_favoriteRestaurant.js
+++ script_favoriteRestaurant.js
@@ -10,12 +10,12 @@ function addFavorite(){
             restaurantId = heart.className.split(' ').pop() // para aceder ao restaurnat id 
             console.log(restaurantId)  
             let request = new XMLHttpRequest()
+
             request.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                // document.getElementById("demo").innerHTML = this.responseText;
                 heart.className = "fas fa-heart "  + restaurantId
             }
-            };
+            }
             console.log(heart) 
             var url = "favorite_restaurant.php?restaurantId="  + restaurantId 
 
